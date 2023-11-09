1e18e9dc65b63e599a26a95b993d6981a70d0ba6
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2023-11-09 22:39:15 +0900

    first commit

---
+++ shell/run_AirSimNH.sh
@@ -0,0 +1,6 @@
+
+
+cd /home/amsl/AirSim/Unreal/Environments/AirSimNH/LinuxNoEditor
+./AirSimNH.sh --ResX=640 -ResY=480 -windowed --settings /home/amsl/AirSim/ros/src/airsim_tutorial_pkgs/settings/front_stereo_and_centor_mono.json
+
+
